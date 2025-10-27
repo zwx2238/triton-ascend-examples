563de81efccb6439f6c428b62f2a1f9f349f88ce
xiaobaicxy <hit_fuyong@163.com>
2025-10-27 21:46:26 +0800

    impl gather & scatter & scatter_wgrad on npu

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import numpy as np
 import torch
+import triton.runtime.driver as driver
 
 
 def is_npu() -> bool:
@@ -11,3 +13,18 @@ def is_cuda():
 
 def next_power_of_2(n: int):
     return 1 << (n - 1).bit_length() if n > 0 else 1
+
+
+def get_npu_properties():
+    device = torch.npu.current_device()
+    return driver.active.utils.get_device_properties(device)
+
+
+def round_up(x: torch.Tensor, value: int):
+    assert isinstance(value, int)
+    assert x.dtype == torch.int32
+    return torch.div(x + (value - 1), value, rounding_mode='trunc') *value
+
+
+def to_numpy(x: torch.Tensor) -> np.ndarray:
+    return x.detach().cpu().numpy()
